83a29ed6afbf33e038ad633e6110ac0f86990282
Sidharth-Singh10 <sidharthsingh714@gmail.com>
2025-04-24 22:44:38 +0530

    added imports

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,17 @@
 import Image from "next/image";
+import * as DAO from "../../packages/dao";
+import * as DaoFactory from "../../packages/dao_factory";
+import * as ProfitDistribution from "../../packages/profit_distribution";
+import * as ProfitToken from "../../packages/profit_token";
 
 export default function Home() {
+  const contract = new DAO.Client({
+    ...DAO.networks.testnet,
+    rpcUrl: "https://soroban-testnet.stellar.org:443",
+  });
+
+  console.log("DAO Client", contract);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
